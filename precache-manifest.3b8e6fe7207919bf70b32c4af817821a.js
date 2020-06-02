d13d5a21cc62886b315119f0b60473206314b418
Carlos Enciso <carlos.enciso.morones@gmail.com>
2020-06-02 17:28:47 -0500

    Updates

---
+++ precache-manifest.3b8e6fe7207919bf70b32c4af817821a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b2e1e4a1289c65d71d05f897c634ac",
+    "url": "/customise-me/index.html"
+  },
+  {
+    "revision": "17870b69de50b006d190",
+    "url": "/customise-me/static/css/main.c9a2b9d9.chunk.css"
+  },
+  {
+    "revision": "c11c0d2fa3348f9df55a",
+    "url": "/customise-me/static/js/2.e6544fb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/customise-me/static/js/2.e6544fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17870b69de50b006d190",
+    "url": "/customise-me/static/js/main.aa6c1191.chunk.js"
+  },
+  {
+    "revision": "f783324b8b517073c3ed",
+    "url": "/customise-me/static/js/runtime-main.1ae2f4b2.js"
+  },
+  {
+    "revision": "63eb2e00fa5e304f7811aab0afd68e7e",
+    "url": "/customise-me/static/media/background1.63eb2e00.jpg"
+  },
+  {
+    "revision": "092c5e09acb80658b15aeb2e7572f8de",
+    "url": "/customise-me/static/media/crossing.092c5e09.png"
+  },
+  {
+    "revision": "5e5a649daa54391a8d1f840f46e0cd32",
+    "url": "/customise-me/static/media/customizeme_final_color.5e5a649d.png"
+  },
+  {
+    "revision": "861372210f6fe15684eaa9b969966e58",
+    "url": "/customise-me/static/media/unnamed.86137221.jpg"
+  }
+]);
